60c15c38f9e2cd97abe2daaba14b259440820fb2
prisha2610 <42508195+prisha2610@users.noreply.github.com>
2020-07-11 16:08:17 +0530

    Add files via upload

--- get_data.m
+++ get_data.m
@@ -7,12 +7,12 @@ days=height(T);
 save('data.mat','S','I','R','-append');
 
 title('Actual');
-plot(1:days,S,'-b');    %displaying susceptible population curve
+%plot(1:days,S,'-b');    %displaying susceptible population curve
 hold on;
 plot(1:days,I,'-r');    %displaying infected population curve
 plot(1:days,R,'-g');   %displaying removed population curve
-legend('Susceptible','Infected','Removed');
-axis([0,days,0,N]);
+legend('Infected','Removed');
+%axis([0,days,0,max(]);
 xlabel('#days');
 ylabel('population');
 hold off;
