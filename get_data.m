c25099e32ba636f158f8fde65a9f5e1bf38a6180
prisha2610 <42508195+prisha2610@users.noreply.github.com>
2020-07-12 17:38:29 +0530

    Add files via upload

--- get_data.m
+++ get_data.m
@@ -1,16 +1,35 @@
 T=readtable('date-India-confirmed-recovered-dead.csv');
-load('data.mat','N');
-R=T.r+T.d;
-I=T.c-R;
-S=N-T.c;
+N=136641750;
+
+R_all=T.r+T.d;
+I_all=T.c-T.r-T.d;
+S_all=N-T.c;
 days=height(T);
-save('data.mat','S','I','R','-append');
+
+
+phase=input('1. Pre-Nationwide Lockdown\n2. Lockdown Period\n3. Post-Nationwide Lockdown\n');
+switch phase
+    case 1
+        S=S_all(41:63);
+        I=I_all(41:63);
+        R=R_all(41:63);
+    case 2
+        S=S_all(64:131);
+        I=I_all(64:131);
+        R=R_all(64:131);
+    case 3
+        S=S_all(132:end);
+        I=I_all(132:end);
+        R=R_all(132:end);
+end
+
+save('data.mat','N','days','S','I','R','phase','S_all','I_all','R_all');
 
 title('Actual');
 %plot(1:days,S,'-b');    %displaying susceptible population curve
 hold on;
-plot(1:days,I,'-r');    %displaying infected population curve
-plot(1:days,R,'-g');   %displaying removed population curve
+plot(1:days,I_all,'-r');    %displaying infected population curve
+plot(1:days,R_all,'-g');   %displaying removed population curve
 legend('Infected','Removed');
 %axis([0,days,0,max(]);
 xlabel('#days');
