bd2ef5ea4977787e1473bd45367997faa415480a
prisha2610 <42508195+prisha2610@users.noreply.github.com>
2020-07-14 15:35:13 +0530

    Add files via upload

--- det_ode.m
+++ det_ode.m
@@ -36,21 +36,22 @@ function [f_spread]=det_ode()
         title('Gillespie Algo + ODE');
         xlabel('#Days');
         ylabel('Population');
+        
+        f2=figure;
+        plot(t,var(:,1),'-b');
+        hold on;
+        plot(t,var(:,2),'-r');
+        plot(t,N-var(:,1)-var(:,2),'-g');
+        legend('Susceptible','Infected','Removed');
+        title('ODE');
+        xlabel('#Days');
+        ylabel('Population');
     else
         id=find(t<=y);
         figure(f1);
         plot(t(id),var(id,2),'-r','DisplayName','ODE');
     end
 
-    f2=figure;
-    plot(t,var(:,1),'-b');
-    hold on;
-    plot(t,var(:,2),'-r');
-    plot(t,N-var(:,1)-var(:,2),'-g');
-    legend('Susceptible','Infected','Removed');
-    title('ODE');
-    xlabel('#Days');
-    ylabel('Population');
     
     f3=figure;
     plot(t,var(:,1),'--b');
